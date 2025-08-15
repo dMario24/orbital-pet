f9e8051270c0a3d5913fecd5138830f4021bfeb4
dMario24 <data.mario24@gmail.com>
2025-08-15 13:58:46 +0900

    v0.1.6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { LandingPage } from '@/components/LandingPage';
 
-export const revalidate = 60; // Revalidate at most every 60 seconds
+export const revalidate = 600; // Revalidate at most every 60 seconds
 
 export default function HomePage() {
   return <LandingPage />;
