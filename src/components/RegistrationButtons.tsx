7060aa7340671d2f29de221e3edfd6168caab661
dMario24 <data.mario24@gmail.com>
2025-08-17 21:00:29 +0900

    참가자 신청 오픈

--- src/components/RegistrationButtons.tsx
+++ src/components/RegistrationButtons.tsx
@@ -15,7 +15,7 @@ export const RegistrationButtons = () => {
   return (
     <div className="flex justify-center gap-4 my-6">
       <a
-        href="#"
+        href="https://onoffmix.com/event/329362"
         onClick={() => handleRegistrationClick('attendee')}
         className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-6 rounded-lg transition-transform transform hover:scale-105"
       >
