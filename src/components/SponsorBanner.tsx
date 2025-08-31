6091804c06cdfced59893e6ecefd738a5bed369e
dMario24 <data.mario24@gmail.com>
2025-08-31 10:19:12 +0900

    후원사 bettercode

--- src/components/SponsorBanner.tsx
+++ src/components/SponsorBanner.tsx
@@ -6,10 +6,47 @@ export const SponsorBanner = () => {
       <div className="text-center">
         <h3 className="text-lg font-bold text-yellow-400 mb-2">Sponsored By</h3>
         <div className="flex justify-center items-center gap-8">
-          <p className="text-gray-300">인사이트마이닝</p>
-          <p className="text-gray-300">테드폴허브</p>
-          <p className="text-gray-300">에스프레소북</p>
-          <p className="text-gray-300">VReRV</p>
+        <a
+            href="http://insightmining.co.kr"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-yellow-300 transition"
+          >
+            인사이트마이닝
+          </a>
+          <a
+            href="http://www.tadpolehub.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-yellow-300 transition"
+          >
+            테드폴허브
+          </a>
+          <a
+            href="https://harubook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-yellow-300 transition"
+          >
+            에스프레소북
+          </a>
+          <a
+            href="https://vrerv.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-yellow-300 transition"
+          >
+            VReRV
+          </a>
+           <p className="text-gray-300">
+            <a
+            href="https://bettercode.kr"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-yellow-300 transition">
+              베터코드
+            </a>
+            </p>
         </div>
       </div>
     </div>
